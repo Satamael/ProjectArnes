609f7d41deeaa22de547343738ac28f2f666c7c6
Satamael <demidvip@gmail.com>
2019-01-26 07:17:08 +0400

    registration form

---
+++ ProjectArnes/ProjectArnes/MainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ProjectArnes
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBacked = new System.Windows.Forms.Panel();
+            this.menuStripVirtical = new System.Windows.Forms.MenuStrip();
+            this.saddaaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saddsadssdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripHorisontal = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelBacked.SuspendLayout();
+            this.menuStripVirtical.SuspendLayout();
+            this.menuStripHorisontal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelBacked
+            // 
+            this.panelBacked.Controls.Add(this.menuStripVirtical);
+            this.panelBacked.Controls.Add(this.menuStripHorisontal);
+            this.panelBacked.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBacked.Location = new System.Drawing.Point(0, 0);
+            this.panelBacked.Name = "panelBacked";
+            this.panelBacked.Size = new System.Drawing.Size(654, 544);
+            this.panelBacked.TabIndex = 0;
+            // 
+            // menuStripVirtical
+            // 
+            this.menuStripVirtical.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStripVirtical.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saddaaToolStripMenuItem,
+            this.saddsadssdToolStripMenuItem});
+            this.menuStripVirtical.Location = new System.Drawing.Point(0, 24);
+            this.menuStripVirtical.Name = "menuStripVirtical";
+            this.menuStripVirtical.Size = new System.Drawing.Size(85, 520);
+            this.menuStripVirtical.TabIndex = 1;
+            this.menuStripVirtical.Text = "menuStrip2";
+            // 
+            // saddaaToolStripMenuItem
+            // 
+            this.saddaaToolStripMenuItem.Name = "saddaaToolStripMenuItem";
+            this.saddaaToolStripMenuItem.Size = new System.Drawing.Size(72, 19);
+            this.saddaaToolStripMenuItem.Text = "saddaa";
+            // 
+            // saddsadssdToolStripMenuItem
+            // 
+            this.saddsadssdToolStripMenuItem.Name = "saddsadssdToolStripMenuItem";
+            this.saddsadssdToolStripMenuItem.Size = new System.Drawing.Size(72, 19);
+            this.saddsadssdToolStripMenuItem.Text = "saddsadssd";
+            // 
+            // menuStripHorisontal
+            // 
+            this.menuStripHorisontal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStripHorisontal.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStripHorisontal.Location = new System.Drawing.Point(0, 0);
+            this.menuStripHorisontal.Name = "menuStripHorisontal";
+            this.menuStripHorisontal.Size = new System.Drawing.Size(654, 24);
+            this.menuStripHorisontal.TabIndex = 0;
+            this.menuStripHorisontal.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(73, 20);
+            this.toolStripMenuItem1.Text = "122121321";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(91, 20);
+            this.toolStripMenuItem2.Text = "214422412442";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 544);
+            this.Controls.Add(this.panelBacked);
+            this.MainMenuStrip = this.menuStripHorisontal;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelBacked.ResumeLayout(false);
+            this.panelBacked.PerformLayout();
+            this.menuStripVirtical.ResumeLayout(false);
+            this.menuStripVirtical.PerformLayout();
+            this.menuStripHorisontal.ResumeLayout(false);
+            this.menuStripHorisontal.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBacked;
+        private System.Windows.Forms.MenuStrip menuStripHorisontal;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.MenuStrip menuStripVirtical;
+        private System.Windows.Forms.ToolStripMenuItem saddaaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saddsadssdToolStripMenuItem;
+    }
+}
+
---
+++ ProjectArnes/ProjectArnes/Registration.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ProjectArnes
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxRePassword = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPromo = new System.Windows.Forms.TextBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelRePassword = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelPromo = new System.Windows.Forms.Label();
+            this.buttonRegistrationEnd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(159, 13);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(132, 22);
+            this.textBoxLogin.TabIndex = 0;
+            this.textBoxLogin.WordWrap = false;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(159, 51);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(132, 22);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // textBoxRePassword
+            // 
+            this.textBoxRePassword.Location = new System.Drawing.Point(159, 79);
+            this.textBoxRePassword.Name = "textBoxRePassword";
+            this.textBoxRePassword.Size = new System.Drawing.Size(132, 22);
+            this.textBoxRePassword.TabIndex = 2;
+            this.textBoxRePassword.TextChanged += new System.EventHandler(this.textBoxRePassword_TextChanged);
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(159, 107);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(132, 22);
+            this.textBoxEmail.TabIndex = 3;
+            // 
+            // textBoxPromo
+            // 
+            this.textBoxPromo.Location = new System.Drawing.Point(159, 148);
+            this.textBoxPromo.Name = "textBoxPromo";
+            this.textBoxPromo.Size = new System.Drawing.Size(132, 22);
+            this.textBoxPromo.TabIndex = 4;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AccessibleDescription = "";
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(12, 18);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(47, 17);
+            this.labelLogin.TabIndex = 5;
+            this.labelLogin.Text = "Логин";
+            this.labelLogin.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(12, 56);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(57, 17);
+            this.labelPassword.TabIndex = 5;
+            this.labelPassword.Text = "Пароль";
+            // 
+            // labelRePassword
+            // 
+            this.labelRePassword.AutoSize = true;
+            this.labelRePassword.Location = new System.Drawing.Point(12, 84);
+            this.labelRePassword.Name = "labelRePassword";
+            this.labelRePassword.Size = new System.Drawing.Size(130, 17);
+            this.labelRePassword.TabIndex = 5;
+            this.labelRePassword.Text = "Повторите пароль";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(12, 112);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(42, 17);
+            this.labelEmail.TabIndex = 5;
+            this.labelEmail.Text = "Email";
+            // 
+            // labelPromo
+            // 
+            this.labelPromo.AutoSize = true;
+            this.labelPromo.Location = new System.Drawing.Point(12, 153);
+            this.labelPromo.Name = "labelPromo";
+            this.labelPromo.Size = new System.Drawing.Size(74, 17);
+            this.labelPromo.TabIndex = 5;
+            this.labelPromo.Text = "Промокод";
+            // 
+            // buttonRegistrationEnd
+            // 
+            this.buttonRegistrationEnd.Location = new System.Drawing.Point(79, 181);
+            this.buttonRegistrationEnd.Name = "buttonRegistrationEnd";
+            this.buttonRegistrationEnd.Size = new System.Drawing.Size(165, 37);
+            this.buttonRegistrationEnd.TabIndex = 6;
+            this.buttonRegistrationEnd.Text = "Зарегистрироваться";
+            this.buttonRegistrationEnd.UseVisualStyleBackColor = true;
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 230);
+            this.Controls.Add(this.buttonRegistrationEnd);
+            this.Controls.Add(this.labelPromo);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelRePassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.textBoxPromo);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxRePassword);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Name = "Registration";
+            this.Text = "Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxRePassword;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPromo;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelRePassword;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelPromo;
+        private System.Windows.Forms.Button buttonRegistrationEnd;
+    }
+}
